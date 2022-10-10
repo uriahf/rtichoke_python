48154eb87b52edfee50e0d3d42230a3a7460d7bd
Uriah Finkel <ufinkel@gmail.com>
2022-10-10 10:38:53 +0300

    Close #2

--- read_curve_dictionary.py
+++ read_curve_dictionary.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import rtichoke
-
+from rtichoke.rtichoke_curves.exported_functions import create_plotly_curve
 
 reference_data = pd.read_csv('C:/Users/CRI_user/Documents/reference_data.csv')
 performance_data_ready_for_curve = pd.read_csv('C:/Users/CRI_user/Documents/performance_data_ready_for_curve.csv')
@@ -13,8 +12,14 @@ group_colors_vec = json.load(f)
 f = open('C:/Users/CRI_user/Documents/axis_ranges.json')
 axis_ranges = json.load(f)
 
+fig_new = create_plotly_curve(
+    {
+        "reference_data" : reference_data,
+        "performance_data_ready_for_curve" : performance_data_ready_for_curve,
+        "group_colors_vec": group_colors_vec,
+        "axis_ranges" : axis_ranges
+    }
+)
 
-
-fig_new = create_plotly_curve(reference_data, performance_data_ready_for_curve, group_colors_vec, axis_ranges)
 fig_new.show()
 
