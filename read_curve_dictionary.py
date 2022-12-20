54476a452c667f240e990af3e10907838b5e34b1
Uriah Finkel <ufinkel@gmail.com>
2022-12-20 07:26:11 +0200

    add roc.py

--- read_curve_dictionary.py
+++ read_curve_dictionary.py
@@ -1,28 +1,61 @@
 import pandas as pd
 from rtichoke.rtichoke_curves.exported_functions import create_plotly_curve
+from rtichoke.rtichoke_curves.roc import create_roc_curve
 import json
+import requests
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import numpy as np
 
-f = open('C:/Users/CRI_user/Documents/rtichoke_curve_json_array.json')
-rtichoke_curve_dict = json.load(f)
-
-rtichoke_curve_dict["reference_data"] = pd.DataFrame.from_dict(rtichoke_curve_dict["reference_data"]) 
-rtichoke_curve_dict["performance_data_ready_for_curve"] = pd.DataFrame.from_dict(rtichoke_curve_dict["performance_data_ready_for_curve"]) 
 
 
-# print(rtichoke_curve_dict["reference_data"])
+lr = LogisticRegression()
+x = np.arange(10).reshape(-1, 1)
+y = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
 
-# not (rtichoke_curve_dict["reference_data"].empty)
+model = LogisticRegression(solver='liblinear', random_state=0)
 
-# print("new rtichoke dict")
-# print(
-# rtichoke_curve_dict["perf_dat_type"] not in ["several models", "several populations"]
-# )
+model.fit(x, y)
 
+roc_curve = create_roc_curve(
+    probs = {'Logistic Regression' :  model.predict_proba(x)[:,1].tolist()},
+    reals = {'Logistic Regression' : y.tolist()}
+)
+
+roc_curve.show()
+
+
+r = requests.post(
+   "http://127.0.0.1:6706/roc_curve_list",  
+   json = {
+    "probs" : {'Logistic Regression' :  model.predict_proba(x)[:,1].tolist()},
+    "reals" : {'Logistic Regression' : y.tolist()} 
+    }
+)
+
+rtichoke_curve_list2 = r.json()
+
+rtichoke_curve_list2
+
+print(rtichoke_curve_list2.keys())
+rtichoke_curve_list2["reference_data"] = pd.DataFrame.from_dict(rtichoke_curve_list2["reference_data"]) 
+
+print(rtichoke_curve_list2['reference_data'])
+
+print(rtichoke_curve_list2['axes_labels'])
+
+print(rtichoke_curve_list2['group_colors_vec'])
+
+rtichoke_curve_list2["performance_data_ready_for_curve"] = pd.DataFrame.from_dict(rtichoke_curve_list2["performance_data_ready_for_curve"]) 
+
+
+print(rtichoke_curve_list2["axes_ranges"])
 
 
 
 fig_new = create_plotly_curve(
-    rtichoke_curve_dict
+    rtichoke_curve_list2
 )
 
 fig_new.show()
