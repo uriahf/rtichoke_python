57cc16fdc7503c67c1efda1cb4838a1cbd912051
urigott <urigott@gmail.com>
2023-07-22 02:33:36 +0300

    fix tests for performance data & verification tests

--- src/tests/test_calculate_performance_data.py
+++ src/tests/test_calculate_performance_data.py
@@ -1,29 +1,29 @@
 import unittest
 import numpy as np
 import pandas as pd
-from numpy import assert_allclose
-from rtichoke.performance_data._performance_data import (
-    prepare_performance_data,
-    prepare_performance_table,
-)
+from numpy.testing import assert_allclose
+from rtichoke import Rtichoke
 
 # UnitTests created with ChatGPT! :-)
 
 
 class TestPreparePerformanceTable(unittest.TestCase):
     def setUp(self):
-        self.probs = np.array([0.7, 0.8, 0.9, 0.4, 0.2, 0.6, 0.5])
-        self.reals = np.array([1, 1, 1, 0, 0, 1, 0])
-        self.by = 0.1
-        self.stratified_by = "probability_threshold"
-        self.pop_name = "pop1"
+        probs = {"pop1": np.array([0.7, 0.8, 0.9, 0.4, 0.2, 0.6, 0.5])}
+        reals = {"pop1": np.array([1, 1, 1, 0, 0, 1, 0])}
+        self.r = Rtichoke(probs=probs, reals=reals, by=0.1)
 
-    def test_prepare_performance_table_returns_dataframe(self):
-        result = prepare_performance_table(self.probs, self.reals)
-        self.assertIsInstance(result, pd.DataFrame)
+    def test_performance_table_type_and_size(self):
+        self.assertIsInstance(self.r.performance_table_pt, pd.DataFrame)
+        self.assertIsInstance(self.r.performance_table_ppcr, pd.DataFrame)
 
-    def test_prepare_performance_data_contains_expected_columns(self):
-        result = prepare_performance_table(self.probs, self.reals)
+        self.assertEqual(self.r.performance_table_pt.shape, (11, 15))
+        self.assertEqual(self.r.performance_table_ppcr.shape, (11, 15))
+
+    def test_performance_data_contains_expected_columns(self):
+        result_cols_pt = self.r.performance_table_pt.columns
+        result_cols_ppcr = self.r.performance_table_ppcr.columns
+
         expected_cols = [
             "Population",
             "probability_threshold",
@@ -41,63 +41,45 @@ class TestPreparePerformanceTable(unittest.TestCase):
             "lift",
             "Net_benefit",
         ]
-        self.assertCountEqual(result.columns, expected_cols)
+        self.assertCountEqual(result_cols_pt, expected_cols)
+        self.assertCountEqual(result_cols_ppcr, expected_cols)
 
-    def test_prepare_performance_data_contains_correct_prob_thresholds(self):
-        result = prepare_performance_table(self.probs, self.reals, by=self.by)
-        expected_prob_thresholds = np.array(
-            [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-        )
-        assert_allclose(
-            result["probability_threshold"].values, expected_prob_thresholds
-        )
+    def test_performance_data_contains_correct_thresholds(self):
+        expected_thresholds = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+        result_thresholds_pt = self.r.performance_table_pt["probability_threshold"]
+        result_thresholds_ppcr = self.r.performance_table_ppcr["ppcr"][-1::-1]
 
-    def test_prepare_performance_data_contains_expected_population_name(self):
-        result = prepare_performance_table(
-            self.probs, self.reals, pop_name=self.pop_name
-        )
-        self.assertTrue((result["Population"] == self.pop_name).all())
+        assert_allclose(result_thresholds_pt, expected_thresholds)
+        assert_allclose(result_thresholds_ppcr, expected_thresholds)
 
+    def test_performance_data_contains_expected_population_name(self):
+        expected_pop_name = ["pop1"]
+        pt_table_pop_name = self.r.performance_table_pt["Population"].unique()
+        ppcr_table_pop_name = self.r.performance_table_ppcr["Population"].unique()
 
-class TestPreparePerformanceData(unittest.TestCase):
-    def test_single_probas_single_labels(self):
-        probas = np.array([0.2, 0.4, 0.6, 0.8])
-        labels = np.array([0, 0, 1, 1])
-        result = prepare_performance_data(probas, labels)
-        self.assertIsInstance(result, pd.DataFrame)
-        self.assertEqual(
-            len(result), 101
-        )  # 101 rows: 100 thresholds + 1 row for overall metrics
-        self.assertEqual(result["Population"].nunique(), 1)  # 1 population
+        self.assertEqual(expected_pop_name, pt_table_pop_name)
+        self.assertEqual(expected_pop_name, ppcr_table_pop_name)
 
-    def test_multiple_probas_single_labels(self):
-        probas1 = np.array([0.2, 0.4, 0.6, 0.8])
-        probas2 = np.array([0.3, 0.5, 0.7, 0.9])
-        labels = np.array([0, 0, 1, 1])
-        result = prepare_performance_data({"pop1": probas1, "pop2": probas2}, labels)
-        self.assertIsInstance(result, pd.DataFrame)
-        self.assertEqual(len(result), 202)  # 2 populations, 101 thresholds each
-        self.assertEqual(result["Population"].nunique(), 2)  # 2 populations
+    def test_performance_data_with_two_populations(self):
+        probs = {
+            "pop1": np.array([0.7, 0.8, 0.9, 0.4, 0.2, 0.6, 0.5]),
+            "pop2": np.array([0.7, 0.8, 0.9, 0.4, 0.2, 0.6, 0.5]),
+        }
+        reals = {
+            "pop1": np.array([1, 1, 1, 0, 0, 1, 0]),
+            "pop2": np.array([1, 1, 1, 0, 0, 1, 0]),
+        }
+        r = Rtichoke(probs=probs, reals=reals, by=0.1)
 
-    def test_multiple_probas_multiple_labels(self):
-        probas1 = np.array([0.2, 0.4, 0.6, 0.8])
-        probas2 = np.array([0.3, 0.5, 0.7, 0.9])
-        labels1 = np.array([0, 0, 1, 1])
-        labels2 = np.array([1, 0, 0, 1])
-        result = prepare_performance_data(
-            {"pop1": probas1, "pop2": probas2}, {"pop1": labels1, "pop2": labels2}
-        )
-        self.assertIsInstance(result, pd.DataFrame)
-        self.assertEqual(
-            len(result), 202
-        )  # 2 populations, 2 label sets, 101 thresholds each
-        self.assertEqual(result["Population"].nunique(), 2)  # 2 populations
+        expected_pop_name = ["pop1", "pop2"]
+        pt_table_pop_name = r.performance_table_pt["Population"].unique()
+        ppcr_table_pop_name = r.performance_table_ppcr["Population"].unique()
 
-    def test_wrong_inputs(self):
-        with self.assertRaises(Exception):
-            prepare_performance_data({"pop1": [0.1, 0.2]}, [0, 1, 1])
-        with self.assertRaises(Exception):
-            prepare_performance_data(1, 2)
+        self.assertEqual(expected_pop_name, pt_table_pop_name)
+        self.assertEqual(expected_pop_name, ppcr_table_pop_name)
+
+        self.assertEqual(r.performance_table_pt.shape, (15, 22))
+        self.assertEqual(r.performance_table_ppcr.shape, (15, 22))
 
 
 if __name__ == "__main__":
--- src/tests/test_vericiation_tests.py
+++ src/tests/test_vericiation_tests.py
@@ -4,11 +4,15 @@ import pandas as pd
 from numpy.testing import assert_array_equal, assert_allclose
 from pandas.testing import assert_frame_equal
 from rtichoke.helpers.validations import *
+from rtichoke import Rtichoke
 
 
 class TestCheckProbs(unittest.TestCase):
     def test_valid_probs(self):
-        self.assertTrue(check_probs(np.array([0.1, 0.3, 0.6, 0.9])))
+        try:
+            check_probs(np.array([0.1, 0.3, 0.6, 0.9]))
+        except Exception as e:
+            self.fail(f"Should return None when OK: {e}")
 
     def test_invalid_probs_low(self):
         with self.assertRaises(Exception):
@@ -22,26 +26,14 @@ class TestCheckProbs(unittest.TestCase):
 ###
 
 
-class TestCheckStratifiedByInput(unittest.TestCase):
-    def test_valid_stratified_by_probability_threshold(self):
-        self.assertTrue(check_stratified_by_input("probability_threshold"))
-
-    def test_valid_stratified_by_ppcr(self):
-        self.assertTrue(check_stratified_by_input("ppcr"))
-
-    def test_invalid_stratified_by(self):
-        with self.assertRaises(Exception):
-            check_stratified_by_input("invalid_stratified_by")
-
-
-###
-
-
 class TestCheckProbsVsReals(unittest.TestCase):
     def test_valid_probs_vs_reals(self):
-        probs = np.array([0.1, 0.3, 0.6, 0.9])
-        reals = np.array([0, 1, 0, 1])
-        self.assertTrue(check_probs_vs_reals(probs, reals))
+        try:
+            probs = np.array([0.1, 0.3, 0.6, 0.9])
+            reals = np.array([0, 1, 0, 1])
+            check_probs_vs_reals(probs, reals)
+        except Exception as e:
+            self.fail(f"Should return None when OK: {e}")
 
     def test_invalid_shape(self):
         probs = np.array([0.1, 0.3, 0.6, 0.9])
@@ -59,8 +51,11 @@ class TestCheckProbsVsReals(unittest.TestCase):
 class TestCheckReals(unittest.TestCase):
     def test_valid_reals(self):
         # Test a valid input
-        reals = np.array([0, 1, 1, 0, 1])
-        self.assertTrue(check_reals(reals))
+        try:
+            reals = np.array([0, 1, 1, 0, 1])
+            check_reals(reals)
+        except Exception as e:
+            self.fail(f"Should return None when OK: {e}")
 
     def test_reals_with_negative_values(self):
         # Test that an exception is raised when reals include negative values
@@ -82,49 +77,59 @@ class TestCheckReals(unittest.TestCase):
 
 
 class TestCheckBy(unittest.TestCase):
-    def test_valid_by(self):
-        # Test a valid input
-        by = 0.25
-        self.assertTrue(check_by(by))
+    def test_valid_by_value(self):
+        r = Rtichoke(
+            probs=np.array([0.1, 0.5, 0.9]), reals=np.array([0, 1, 0]), by=0.25
+        )
 
     def test_by_not_a_float(self):
         # Test that an exception is raised when the input is not a float
-        by = "0.25"
-        with self.assertRaises(Exception):
-            check_by(by)
+        with self.assertRaises(Exception) as e:
+            r = Rtichoke(
+                probs=np.array([0.1, 0.5, 0.9]), reals=np.array([0, 1, 0]), by="0.25"
+            )
+        self.assertEqual(
+            str(e.exception), "Argument `by` must be a float,  0 > `by` <= 0.5"
+        )
 
     def test_by_out_of_range(self):
         # Test that an exception is raised when the input is out of range
-        by = 0.6
-        with self.assertRaises(Exception):
-            check_by(by)
-
-    def test_check_stratified_by_input(self):
-        # test for valid input
-        self.assertTrue(check_stratified_by_input("ppcr"))
-
-        # test for invalid input
-        with self.assertRaises(Exception):
-            check_stratified_by_input("random")
+        with self.assertRaises(Exception) as e:
+            r = Rtichoke(
+                probs=np.array([0.1, 0.5, 0.9]), reals=np.array([0, 1, 0]), by="0.6"
+            )
+        self.assertEqual(
+            str(e.exception), "Argument `by` must be a float,  0 > `by` <= 0.5"
+        )
 
 
 class TestValidateInputs(unittest.TestCase):
     def test_validate_inputs(self):
+        r = Rtichoke(probs=np.array([0.1, 0.5, 0.9]), reals=np.array([0, 1, 0]), by=0.2)
+
         # test for valid input
-        self.assertIsNone(
-            validate_inputs(np.array([0.1, 0.5, 0.9]), np.array([0, 1, 0]), 0.1, "ppcr")
+        probs = np.array([0.1, 0.5, 0.9])
+        reals = np.array([0, 1, 0])
+        self.assertIsNone(r.validate_inputs(probs, reals))
+
+        # test for un-equal sized arrays
+        with self.assertRaises(Exception) as e:
+            r.validate_inputs(np.array([0.1, 0.5]), np.array([0, 1, 0]))
+        self.assertEqual(
+            str(e.exception),
+            "Probs and reals shapes are inconsistent ((2,) and (3,))",
         )
 
-        # test for invalid input
-        with self.assertRaises(Exception):
-            validate_inputs(np.array([0.1, 0.5]), np.array([0, 1, 0]), 0.1, "ppcr")
-        with self.assertRaises(Exception):
-            validate_inputs(
-                np.array([-0.1, 0.5, 1.1]), np.array([0, 1, 0]), 0.1, "ppcr"
-            )
-        with self.assertRaises(Exception):
-            validate_inputs(np.array([0.1, 0.5, 0.9]), np.array([0, 1]), 0.1, "ppcr")
-        with self.assertRaises(Exception):
-            validate_inputs(np.array([0.1, 0.5, 0.9]), np.array([0, 1, 0]), 0, "ppcr")
-        with self.assertRaises(Exception):
-            validate_inputs(np.array([0.1, 0.5, 0.9]), np.array([0, 1, 0]), 0.6)
+        # test for invalid probs
+        with self.assertRaises(Exception) as e:
+            r.validate_inputs(np.array([-0.1, 0.5, 1.1]), np.array([0, 1, 0]))
+        self.assertEqual(str(e.exception), "Probs must be within [0, 1]")
+
+        # test for invalid reals
+        with self.assertRaises(Exception) as e:
+            r.validate_inputs(np.array([0.1, 0.5, 0.9]), np.array([0, 1, 2]))
+        self.assertEqual(str(e.exception), "Reals must include only 0's and 1's")
+
+
+if __name__ == "__main__":
+    unittest.main()
