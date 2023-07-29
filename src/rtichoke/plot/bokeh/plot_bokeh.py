cba7c1dcf8d888cdb14995c852c7c31ae412d9f4
urigott <urigott@gmail.com>
2023-07-29 20:18:08 +0300

    plot_bokeh.py: add histogram plot below calibration plot

--- src/rtichoke/plot/bokeh/plot_bokeh.py
+++ src/rtichoke/plot/bokeh/plot_bokeh.py
@@ -1,4 +1,4 @@
-from bokeh.layouts import column
+from bokeh.layouts import column, gridplot
 from bokeh.models import (
     ColumnDataSource,
     Slider,
@@ -6,8 +6,11 @@ from bokeh.models import (
     HoverTool,
     CDSView,
     GroupFilter,
+    BoxZoomTool,
+    Range1d,
 )
 from bokeh.plotting import figure, output_file, save, show
+import numpy as np
 
 # from collections import OrderedDict
 from .create_bokeh_plot_dict import *
@@ -15,6 +18,11 @@ from .create_bokeh_plot_dict import *
 
 def plot_bokeh(self, generic_plot_dict, filename=None):
     curve_type = generic_plot_dict["curve_type"]
+
+    if curve_type == "calibration":
+        plot_bokeh_calibration(self, generic_plot_dict, filename=filename)
+        return None
+
     stratification = generic_plot_dict["stratification"]
 
     graph_meta = create_bokeh_plot_dict(generic_plot_dict)
@@ -122,6 +130,103 @@ def plot_bokeh(self, generic_plot_dict, filename=None):
     plot.xaxis.axis_label = graph_meta["xlabel"]
     plot.yaxis.axis_label = graph_meta["ylabel"]
     layout = column(slider, plot)
+
+    if filename:
+        save(layout)
+    else:
+        show(layout)
+
+
+def plot_bokeh_calibration(self, generic_plot_dict, filename=None):
+    curve_type = "calibration"
+    graph_meta = create_bokeh_plot_dict(generic_plot_dict)
+    x = graph_meta["x"]
+    y = graph_meta["y"]
+    df = self.calibration_table
+    pops, colors, _ = create_pops_and_colors(df)
+
+    # Create plots and widgets
+    if filename:
+        output_file(filename)
+
+    plot = figure(
+        title=f"{curve_type} curve",
+        tools=[BoxZoomTool(match_aspect=True)],
+    )
+
+    histogram = figure(aspect_ratio=3.5, tools=plot.tools, x_range=plot.x_range)
+
+    lines_source = []
+    lines = []
+    circles = []
+    histograms = []
+
+    for j, pop in enumerate(pops):
+        lines_source.append(ColumnDataSource(data=df[df["Population"] == pop]))
+        lines.append(
+            plot.line(
+                x=x,
+                y=y,
+                line_color=colors[j],
+                legend_label=pop,
+                name=pop,
+                line_width=3,
+                line_alpha=0.75,
+                source=lines_source[j],
+            )
+        )
+        circles.append(
+            plot.circle(
+                x=x,
+                y=y,
+                line_color="black",
+                fill_color=colors[j],
+                fill_alpha=0.5,
+                size=8,
+                legend_label=pop,
+                source=lines_source[j],
+            )
+        )
+
+        probs = self.probs[pop] if pop in self.probs.keys() else self.probs
+        hist, edges = np.histogram(probs, bins="auto")
+
+        histograms.append(
+            histogram.quad(
+                top=hist,
+                bottom=0,
+                left=edges[:-1],
+                right=edges[1:],
+                line_color="black",
+                alpha=0.5,
+                name=pop,
+                fill_color=colors[j],
+            )
+        )
+
+    plot.line(
+        graph_meta["reference"]["x"],
+        graph_meta["reference"]["y"],
+        line_width=1,
+        color="gray",
+    )
+
+    plot.legend.location = graph_meta["legend"]
+    plot.legend.click_policy = "hide"
+
+    # add hover data
+    hover = HoverTool(
+        renderers=circles,
+        tooltips=graph_meta["hover_info"],
+    )
+    plot.add_tools(hover)
+
+    # customize plots and finish
+    plot.xaxis.axis_label = graph_meta["xlabel"]
+    plot.yaxis.axis_label = graph_meta["ylabel"]
+
+    link_legend_glyphs(plot, [histogram])
+    layout = gridplot([[plot], [histogram]])
 
     if filename:
         save(layout)
