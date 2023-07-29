86e3ef3f24729c12637969379fc39386d58555d1
urigott <urigott@gmail.com>
2023-07-29 20:13:21 +0300

    update __init__.py: update sub-packages imports

--- src/rtichoke/__init__.py
+++ src/rtichoke/__init__.py
@@ -10,15 +10,25 @@ from .helpers.helper_functions import *
 
 class Rtichoke:
     # import methods
-    from .performance_data._performance_data import (
+    from .performance_data.prepare_performance_data import (
         prepare_performance_data,
         prepare_performance_table,
     )
+    from .performance_data.prepare_calibration_data import (
+        prepare_calibration_data,
+        prepare_calibration_table,
+    )
+
     from .helpers.validations import validate_inputs, validate_plot_inputs, check_by
-    from .helpers.helper_functions import tprint, select_data_table
+    from .helpers.helper_functions import (
+        select_data_table,
+        modified_calibration_curve,
+    )
     from .plot.plotting import plot
 
-    def __init__(self, probs=None, reals=None, by=0.01):
+    def __init__(
+        self, probs=None, reals=None, by=0.01, cal_n_bins=10, cal_strategy="quantile"
+    ):
         super().__init__()
 
         self.probs = probs
@@ -26,7 +36,9 @@ class Rtichoke:
         self.by = by
         self.performance_table_pt = None
         self.performance_table_ppcr = None
+        self.calibration_table = None
         self.prevalence = {}
+        self.N = {}
 
         self.check_by()
 
@@ -38,4 +50,9 @@ class Rtichoke:
         tprint("Calculating performance table stratified by ppcr")
         self.performance_table_ppcr = self.prepare_performance_data(
             stratified_by="ppcr"
+        )
+
+        tprint("Calculating calibration data")
+        self.calibration_table = self.prepare_calibration_data(
+            n_bins=cal_n_bins, strategy=cal_strategy
         )
