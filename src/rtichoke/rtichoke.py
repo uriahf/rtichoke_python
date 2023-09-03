bf83ab96d5f30719043d908e13707eca2b2289bd
urigott <45831390+urigott@users.noreply.github.com>
2023-09-03 15:22:00 +0300

    Update rtichoke.py

--- src/rtichoke/rtichoke.py
+++ src/rtichoke/rtichoke.py
@@ -1,6 +1,6 @@
 """rtichoke is a package for interactive vizualization of performance metrics
 """
-from .helpers.helper_functions import *
+from .helpers.helper_functions import tprint
 
 class Rtichoke:
     """Main Rtichoke class"""
