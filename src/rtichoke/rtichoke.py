b6774212a37d659a3c542f3bdaf2b2434fe5744d
urigott <45831390+urigott@users.noreply.github.com>
2023-09-03 15:09:03 +0300

    Update rtichoke.py

--- src/rtichoke/rtichoke.py
+++ src/rtichoke/rtichoke.py
@@ -3,7 +3,25 @@
 
 class Rtichoke:
     """Main Rtichoke class"""
-
+    # import methods
+    from .performance_data.prepare_performance_data import (
+        prepare_performance_data,
+        prepare_performance_table,
+    )
+    from .performance_data.prepare_calibration_data import (
+        prepare_calibration_data,
+        prepare_calibration_table,
+    )
+    
+    from .helpers.validations import validate_inputs, validate_plot_inputs, check_by
+    from .helpers.helper_functions import (
+        select_data_table,
+        modified_calibration_curve,
+    )
+    from .plot.plotting import plot
+    
+    from .rtichoke import Rtichoke
+    
     def __init__(
         self, probs=None, reals=None, by=0.01, cal_n_bins=10, cal_strategy="quantile"
     ):
