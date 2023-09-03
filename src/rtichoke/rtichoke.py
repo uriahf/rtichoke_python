f183bb5e0e5f24c4e445865f45ffb9a30f66b1a2
urigott <45831390+urigott@users.noreply.github.com>
2023-09-03 15:04:10 +0300

    Update rtichoke.py

--- src/rtichoke/rtichoke.py
+++ src/rtichoke/rtichoke.py
@@ -1,32 +1,8 @@
 """rtichoke is a package for interactive vizualization of performance metrics
 """
 
-from importlib.metadata import version
-
-# __version__ = version("rtichoke")
-
-from .helpers.helper_functions import *
-
-
 class Rtichoke:
     """Main Rtichoke class"""
-
-    # import methods
-    from .performance_data.prepare_performance_data import (
-        prepare_performance_data,
-        prepare_performance_table,
-    )
-    from .performance_data.prepare_calibration_data import (
-        prepare_calibration_data,
-        prepare_calibration_table,
-    )
-
-    from .helpers.validations import validate_inputs, validate_plot_inputs, check_by
-    from .helpers.helper_functions import (
-        select_data_table,
-        modified_calibration_curve,
-    )
-    from .plot.plotting import plot
 
     def __init__(
         self, probs=None, reals=None, by=0.01, cal_n_bins=10, cal_strategy="quantile"
