5da71eee5e7b7e52b45f7449501fb0c4f4e3922a
Uriah Finkel <Ufinkel@gmail.com>
2025-04-06 18:48:21 +0300

    feat: try to run `render_summary_report()`

--- docs/sandbox.py
+++ docs/sandbox.py
@@ -1,35 +1,9 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from lifelines import AalenJohansenFitter
+from rtichoke.summary_report.summary_report import render_summary_report
 
-df_time_to_cancer_dx = pd.read_csv(
-    "https://raw.githubusercontent.com/ddsjoberg/dca-tutorial/main/data/df_time_to_cancer_dx.csv"
-)
 
-reals_mapping = {
-    "censor": 0,
-    "diagnosed with cancer": 1,
-    "dead other causes": 2
-}
-df_time_to_cancer_dx['reals'] = df_time_to_cancer_dx['cancer_cr'].map(reals_mapping)
+help(render_summary_report)
 
-ajf_primary = AalenJohansenFitter()
-ajf_primary.fit(
-    durations=df_time_to_cancer_dx['ttcancer'],
-    event_observed=df_time_to_cancer_dx['reals'],
-    event_of_interest=1
-)
-
-ajf_competing = AalenJohansenFitter()
-ajf_competing.fit(
-    durations=df_time_to_cancer_dx['ttcancer'],
-    event_observed=df_time_to_cancer_dx['reals'],
-    event_of_interest=2
-)
-
-ajf_competing.predict(1)
-
-type(ajf_competing.predict([1]))
-
-ajf_competing.plot_cumulative_density()
-plt.show()
+probs = [0.1, 0.4, 0.8]
+reals = [0, 1, 1]
+times = [1, 3, 5]
+render_summary_report(probs, reals, times)
