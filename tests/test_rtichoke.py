201c13240a8c703925198947b71b22cc6175996b
Uriah Finkel <ufinkel@gmail.com>
2025-06-25 22:15:02 +0300

    chore: close #66

--- tests/test_rtichoke.py
+++ tests/test_rtichoke.py
@@ -18,12 +18,12 @@ from rtichoke.helpers.sandbox_observable_helpers import create_aj_data
 def test_create_aj_data() -> None:
     df = polars.DataFrame(
         {
-            "strata": ["group1"] * 4,
-            "reals": [0, 1, 0, 2],
-            "times": [1, 2, 3, 4],
+            "strata": ["group1"] * 5,
+            "reals": [0, 1, 2, 1, 0],
+            "times": [5, 3, 1, 4, 2],
         }
     )
-    horizons = [2, 4]
+    horizons = [1, 2, 3]
 
     result = create_aj_data(
         df,
@@ -34,14 +34,15 @@ def test_create_aj_data() -> None:
 
     expected = polars.DataFrame(
         {
-            "strata": ["group1", "group1"],
-            "fixed_time_horizon": [2, 4],
-            "real_negatives_est": [8 / 3, 0.0],
-            "real_positives_est": [4 / 3, 4 / 3],
-            "real_competing_est": [0.0, 8 / 3],
-            "real_censored_est": [0.0, 0.0],
-            "censoring_assumption": ["adjusted", "adjusted"],
+            "strata": ["group1", "group1", "group1"],
+            "fixed_time_horizon": [1, 2, 3],
+            "real_negatives_est": [4.0, 4.0, 8 / 3],
+            "real_positives_est": [0.0, 0.0, 4 / 3],
+            "real_competing_est": [1.0, 1.0, 1.0],
+            "real_censored_est": [0.0, 0.0, 0.0],
+            "censoring_assumption": ["adjusted", "adjusted", "adjusted"],
             "competing_assumption": [
+                "adjusted_as_negative",
                 "adjusted_as_negative",
                 "adjusted_as_negative",
             ],
