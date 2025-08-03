ce6592ab8269bc376d00758a651d76a103166441
Uriah Finkel <ufinkel@gmail.com>
2025-08-03 07:10:46 +0300

    tests: close #144

--- tests/test_rtichoke.py
+++ tests/test_rtichoke.py
@@ -5,12 +5,19 @@ A module for tests
 from rtichoke.helpers.sandbox_observable_helpers import (
     create_aj_data,
     extract_aj_estimate_for_strata,
+    assign_and_explode_polars,
+    _aj_adjusted_events,
+    _excluded_events_df,
 )
 
 # from rtichoke import rtichoke
 import polars as pl
 from polars.testing import assert_frame_equal
 import pytest
+
+TIMES = [24.1, 9.7, 49.9, 18.6, 34.8, 14.2, 39.2, 46.0, 4.3, 31.5]
+REALS = [1, 1, 1, 1, 0, 2, 1, 2, 0, 1]
+TIME_HORIZONS = [10.0, 30.0, 50.0]
 
 
 def _expected(
@@ -191,4 +198,156 @@ def test_extract_aj_estimate_for_strata_basic() -> None:
         }
     )
 
+    assert_frame_equal(result, expected)
+
+
+AJ_EXPECTED = {
+    ("adjusted", "adjusted_as_negative"): [
+        (8.88888888888889, 1.1111111111111112, 0.0),
+        (5.555555555555555, 3.3333333333333335, 1.1111111111111112),
+        (0.0, 7.407407407407407, 2.5925925925925926),
+    ],
+    ("adjusted", "adjusted_as_censored"): [
+        (8.88888888888889, 1.1111111111111112, 0.0),
+        (6.349206349206349, 3.6507936507936507, 0.0),
+        (0.0, 10.0, 0.0),
+    ],
+    ("adjusted", "adjusted_as_composite"): [
+        (8.88888888888889, 1.1111111111111112, 0.0),
+        (5.555555555555555, 4.444444444444445, 0.0),
+        (0.0, 10.0, 0.0),
+    ],
+    ("adjusted", "excluded"): [
+        (8.88888888888889, 1.1111111111111112, 0.0),
+        (5.625, 3.375, 0.0),
+        (0.0, 8.0, 0.0),
+    ],
+    ("excluded", "adjusted_as_negative"): [
+        (8.0, 1.0, 0.0),
+        (5.0, 3.0, 1.0),
+        (0.0, 6.0, 2.0),
+    ],
+    ("excluded", "adjusted_as_censored"): [
+        (8.0, 1.0, 0.0),
+        (5.714285714285714, 3.2857142857142856, 0.0),
+        (0.0, 8.0, 0.0),
+    ],
+    ("excluded", "adjusted_as_composite"): [
+        (8.0, 1.0, 0.0),
+        (5.0, 4.0, 0.0),
+        (0.0, 8.0, 0.0),
+    ],
+    ("excluded", "excluded"): [
+        (8.0, 1.0, 0.0),
+        (5.0, 3.0, 0.0),
+        (0.0, 6.0, 0.0),
+    ],
+}
+
+EXCLUDED_EXPECTED = {
+    "adjusted": [0.0, 0.0, 0.0],
+    "excluded": [1.0, 1.0, 2.0],
+}
+
+COMPETING_EXCLUDED = {
+    "excluded": [0.0, 1.0, 2.0],
+    "adjusted_as_negative": [0.0, 0.0, 0.0],
+    "adjusted_as_censored": [0.0, 0.0, 0.0],
+    "adjusted_as_composite": [0.0, 0.0, 0.0],
+}
+
+
+def _expected_aj_df(neg, pos, comp, include_comp=True):
+    estimate_origin_enum = pl.Enum(["fixed_time_horizons", "event_table"])
+
+    data = {
+        "strata": ["group1"] * 3,
+        "fixed_time_horizon": TIME_HORIZONS,
+        "real_negatives_est": [neg[0], neg[1], neg[2]],
+        "real_positives_est": [pos[0], pos[1], pos[2]],
+        "estimate_origin": pl.Series(
+            ["fixed_time_horizons"] * 3, dtype=estimate_origin_enum
+        ),
+    }
+    if include_comp:
+        data["real_competing_est"] = [comp[0], comp[1], comp[2]]
+
+    cols = [
+        "strata",
+        "fixed_time_horizon",
+        "real_negatives_est",
+        "real_positives_est",
+    ]
+    if include_comp:
+        cols.append("real_competing_est")
+    cols.append("estimate_origin")
+
+    return pl.DataFrame(data)[cols]
+
+
+def _expected_excluded_df(censoring, competing):
+    return pl.DataFrame(
+        {
+            "strata": ["group1"] * 3,
+            "fixed_time_horizon": TIME_HORIZONS,
+            "real_censored_est": EXCLUDED_EXPECTED[censoring],
+            "real_competing_est": COMPETING_EXCLUDED[competing],
+        }
+    )
+
+
+@pytest.mark.parametrize(
+    "censoring, competing",
+    [
+        (c, cc)
+        for c in ["adjusted", "excluded"]
+        for cc in [
+            "adjusted_as_negative",
+            "adjusted_as_censored",
+            "adjusted_as_composite",
+            "excluded",
+        ]
+    ],
+)
+def test_aj_adjusted_events(censoring: str, competing: str) -> None:
+    df = pl.DataFrame(
+        {"strata": ["group1"] * len(TIMES), "reals": REALS, "times": TIMES}
+    )
+    exploded = assign_and_explode_polars(df, TIME_HORIZONS)
+    result = _aj_adjusted_events(
+        df, exploded, censoring, competing, TIME_HORIZONS, full_event_table=False
+    ).sort("fixed_time_horizon")
+
+    neg = [v[0] for v in AJ_EXPECTED[(censoring, competing)]]
+    pos = [v[1] for v in AJ_EXPECTED[(censoring, competing)]]
+    comp_vals = [v[2] for v in AJ_EXPECTED[(censoring, competing)]]
+    include_comp = competing != "excluded"
+    expected = _expected_aj_df(neg, pos, comp_vals, include_comp)
+    assert_frame_equal(result, expected)
+
+
+@pytest.mark.parametrize(
+    "censoring, competing",
+    [
+        (c, cc)
+        for c in ["adjusted", "excluded"]
+        for cc in [
+            "adjusted_as_negative",
+            "adjusted_as_censored",
+            "adjusted_as_composite",
+            "excluded",
+        ]
+    ],
+)
+def test_excluded_events_df(censoring: str, competing: str) -> None:
+    df = pl.DataFrame(
+        {"strata": ["group1"] * len(TIMES), "reals": REALS, "times": TIMES}
+    )
+    exploded = assign_and_explode_polars(df, TIME_HORIZONS)
+
+    result = _excluded_events_df(exploded, censoring, competing).sort(
+        "fixed_time_horizon"
+    )
+
+    expected = _expected_excluded_df(censoring, competing)
     assert_frame_equal(result, expected)
