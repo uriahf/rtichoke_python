ebade8d62230e27fcc8c537972b48b2f354fc998
Uriah Finkel <Ufinkel@gmail.com>
2026-08-11 18:11:04 +0300

    route calibration through normalized population API

--- src/rtichoke/calibration/__init__.py
+++ src/rtichoke/calibration/__init__.py
@@ -2,6 +2,6 @@
 Subpackage for Calibration
 """
 
-from .calibration import create_calibration_curve, create_calibration_curve_times
+from .population_calibration import create_calibration_curve, create_calibration_curve_times
 
 __all__ = ["create_calibration_curve", "create_calibration_curve_times"]
